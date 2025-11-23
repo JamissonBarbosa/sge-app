9c389eb1c08784451fb607e9fe408d00c7a653c2
JamissonBarbosa <jamissondasilvafilho@gmail.com>
2025-11-23 20:28:50 -0300

    chore: create apps

--- app/settings.py
+++ app/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'brands',
+    'categories',
+    'supliers',
+    'products',
+    'inflows',
+    'outflows',
 ]
 
 MIDDLEWARE = [
--- categories/models.py
+++ categories/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
-
-class Brand(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=500)
     description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
